cc2cb90beacb65f296e6716c5dae7374831133d1
CoolZonee <jiekelim321@gmail.com>
2021-08-11 21:49:17 +0800

    Update gitignore, baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://personalwebbackend.herokuapp.com/api',
     header: {
         "Content-type": "application/json"
     }
